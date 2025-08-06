98a6c37fa651c155e35b990aa85b32fea738a12a
ludihan <65617704+ludihan@users.noreply.github.com>
2025-08-06 20:45:22 -0300

    8 improve camera (#9)
    * camera with mouse
    
    * now with sprite

--- src/init.lua
+++ src/init.lua
@@ -1,8 +1,8 @@
-local M    = {}
+local M      = {}
 
-local input    = require("src.input")
-local map      = require("src.map")
-local player   = require("src.player")
+local input  = require("src.input")
+local map    = require("src.map")
+local player = require("src.player")
 
 function M.load()
     player.load()
@@ -20,12 +20,29 @@ function M.draw()
         player.camera.x,
         player.camera.y
     )
+    love.graphics.scale(input.zoom, input.zoom)
     map.draw()
     player.draw()
 end
 
 function M.keypressed(key)
     input.keypressed(key)
+end
+
+function M.mousepressed(x, y, button, istouch, presses)
+    input.mousepressed(x, y, button, istouch, presses)
+end
+
+function M.mousereleased(x, y, button, istouch, presses)
+    input.mousereleased(x, y, button, istouch, presses)
+end
+
+function M.mousemoved(x, y, dx, dy, istouch)
+    input.mousemoved(x, y, dx, dy, istouch)
+end
+
+function M.wheelmoved(x, y)
+    input.wheelmoved(x, y)
 end
 
 return M
--- src/player.lua
+++ src/player.lua
@@ -1,32 +1,64 @@
-local M      = {}
+local M          = {}
 
-M.position   = {
+M.position       = {
     x = 0,
     y = 0,
 }
 
-M.camera     = {
+M.camera         = {
     x = 0,
     y = 0,
 }
 
-M.moveStep   = 50
-M.cameraStep = 20
+M.moveStep       = 64
+M.cameraStep     = 20
+
+local image
+local animations = {}
+M.currentAnimation = 1
+M.animationTime = 0
+local animationSwitchTime = 0.5
+M.flip = false
 
 function M.load()
-
+    image = love.graphics.newImage("asset/image/miko.png")
+    image:setFilter("nearest", "nearest")
+    for i = 0, 128, 64 do
+        table.insert(
+            animations,
+            love.graphics.newQuad(i, 0, 64, 64, image)
+        )
+    end
 end
 
 function M.update(dt)
+    M.animationTime = M.animationTime + dt
+    if M.animationTime >= animationSwitchTime then
+        M.animationTime = 0
+        M.currentAnimation = math.fmod(M.currentAnimation, 2) + 1
+    end
 end
 
 function M.draw(dt)
-    love.graphics.rectangle(
-        "fill",
+    local xs = 1
+    local offset = 0
+    if M.flip then
+        xs = -1
+        offset = 64
+    else
+        xs = 1
+        offset = 0
+    end
+    print(M.position.x)
+    love.graphics.draw(
+        image,
+        animations[M.currentAnimation],
         M.position.x,
         M.position.y,
-        M.moveStep,
-        M.moveStep
+        nil,
+        xs,
+        1,
+        offset
     )
 end
 
